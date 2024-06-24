24bcbea43545352e91b1f638ab5aebabd287f20a
jimmyboy722 <jeckman722@gmail.com>
2024-06-24 06:32:31 -0400

    changed some form html id names and adjusted styling to form css

--- assets/JS/form.js
+++ assets/JS/form.js
@@ -1,3 +1,7 @@
+// SELECTING FIRST FORM ELEMENT IN FORM.HTML
+const form = document.querySelector("form");
+
+const blogPosting = function () {};
 // VARIABLES FOR ITEMS IN FORM.HTML FILE
 const blogPosts = document.getElementById("blogPosts").value.trim();
 const username = document.getElementById("u-name").value.trim();
